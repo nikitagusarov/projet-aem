c83a54899886a953ea47219cb401f537cab56cfb
Nik <bernardodelapaz@gmail.com>
2019-10-16 15:07:17 +0200

    Merged database for pesticides + resize

--- Code.R
+++ Code.R
@@ -26,15 +26,44 @@ write.csv(vin, file = "Donnees_ref/vin-p.csv")
 ## End first changes  
 
 ##loading data
-usage=read.csv("C:/Users/jupiter/Documents/Blanc Arnaud/master MIASHS C2ES/M2/analyse empirique de marché/pesticides/info/usages_des_produits_autorises_v2_utf8.csv", sep = ";")
+# usage=read.csv("C:/Users/jupiter/Documents/Blanc Arnaud/master MIASHS C2ES/M2/analyse empirique de marchï¿½/pesticides/info/usages_des_produits_autorises_v2_utf8.csv", sep = ";")
 write.csv(usage, file = "Donnees_ref/usage_pa.csv")
-superficie=read.csv("C:/Users/jupiter/Documents/projet-aem-s/Donnees/superficie départementale par an de vignes.csv", sep = ";", skip = 7, header = T)
+# superficie=read.csv("C:/Users/jupiter/Documents/projet-aem-s/Donnees/superficie dï¿½partementale par an de vignes.csv", sep = ";", skip = 7, header = T)
 write.csv(superficie, file = "Donnees_ref/superficie_vignes.csv")
 View(superficie)
 
-#Construction de la table pesticides
-
-pesticide=left_join(bnvd, usage, by=c("amm"="X.produit..numero.AMM"))
-
-
-
+## Loading packages
+require(tidyverse)
+## Loading data
+usage = read.csv("Donnees_ref/usage_pa.csv")
+View(usage)
+dim(usage) # testing line
+## identifiant.usage
+usage.v = usage %>% 
+    filter(str_detect(identifiant.usage, "Vigne"))
+dim(usage.v) # succesfull filtering
+## Preparation for concat
+names(usage.v)[3]
+class(usage.v$X.produit..numero.AMM)
+usage.v = usage.v %>% 
+    mutate(amm = as.character(X.produit..numero.AMM))
+head(usage.v$amm)
+## Loading second df to merge
+bnvd = read.csv("Donnees_ref/bnvd-vp.csv")
+bnvd = bnvd %>% 
+    mutate(amm = as.character(amm))
+View(bnvd) # verification
+head(bnvd$amm)
+## Merge
+dim(bnvd) # ref
+dim(usage.v) # ref
+bnvdvin = left_join(bnvd, usage.v, by = "amm")
+View(bnvdvin) # verification
+## Clearing
+bnvdvin.n = bnvdvin[is.na(bnvdvin$X.produit..numero.AMM) == F,]
+View(bnvdvin.n) # verif
+names(bnvdvin.n)
+bnvdvin.n = bnvdvin.n[, c(1:7, 16:18, 20:32)]
+## Find a way to reduce data weight
+## Saving
+write.csv(bnvdvin.n, file = "Donnees_ref/bnvdvin.csv")
