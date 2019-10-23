7bf8ff5dbc58e37e3592335dda4f2fee01a6cfe7
Nik <bernardodelapaz@gmail.com>
2019-10-23 15:09:55 +0200

    Replacing mean() by sum()

--- Code.R
+++ Code.R
@@ -265,3 +265,45 @@ vxp %>% filter(quantite_produit < 1000000) %>%
     xlab("Quantité des pésticides") +
     ylab("Surface des vignes")
 dev.off()
+
+png(filename="Graphiques/QvinQpest2.png", width = 600, height = 600)
+vxp %>% filter(quantite_produit < 1000000) %>% 
+    group_by(annee, number) %>%
+    summarise(qq_total = mean(qq_total), quantite_produit = sum(quantite_produit)) %>%
+    ggplot(aes(y = qq_total, x = as.numeric(quantite_produit))) +
+    geom_point() +
+    xlab("Quantité des pésticides") +
+    ylab("Quantité de vin produit")
+dev.off()
+
+png(filename="Graphiques/SvinQpest2.png", width = 600, height = 600)
+vxp %>% filter(quantite_produit < 1000000) %>% 
+    group_by(annee, number) %>%
+    summarise(surface = mean(surface), quantite_produit = sum(quantite_produit)) %>%
+    ggplot(aes(y = surface, x = as.numeric(quantite_produit))) +
+    geom_point() +
+    xlab("Surface des vignes") +
+    ylab("Quantité de vin produit")
+dev.off()
+
+png(filename="Graphiques/QvinQpestAnnee2.png", width = 600, height = 600)
+vxp %>% filter(quantite_produit < 1000000) %>% 
+    group_by(annee, number) %>%
+    summarise(qq_total = mean(qq_total), quantite_produit = sum(quantite_produit)) %>%
+    ggplot(aes(y = qq_total, x = as.numeric(quantite_produit), col = as.factor(annee))) +
+    geom_point() +
+    geom_smooth(method = "lm", se = F) + 
+    xlab("Quantité des pésticides") +
+    ylab("Quantité de vin produit")
+dev.off()
+
+png(filename="Graphiques/QvinQpestRegion2.png", width = 600, height = 600)
+vxp %>% filter(quantite_produit < 1000000) %>% 
+    group_by(annee, number) %>%
+    summarise(qq_total = mean(qq_total), quantite_produit = sum(quantite_produit)) %>%
+    ggplot(aes(y = qq_total, x = as.numeric(quantite_produit), col = as.factor(number))) +
+    geom_point() +
+    geom_smooth(method = "lm", se = F) + 
+    xlab("Quantité des pésticides") +
+    ylab("Quantité de vin produit")
+dev.off()
